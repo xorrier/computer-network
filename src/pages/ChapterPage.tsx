e7ba1f557b1d9fdf6ae28665f450b0d3affd59f1
xorrier <sharmaamrit812@gmail.com>
2026-07-03 00:46:57 +0530

    fix: add key prop to ChapterView component for improved rendering

--- src/pages/ChapterPage.tsx
+++ src/pages/ChapterPage.tsx
@@ -29,7 +29,7 @@ export function ChapterPage() {
           <p>{chapter.promise}</p>
         </div>
       </div>
-      <ChapterView chapter={chapter} />
+      <ChapterView key={chapter.slug} chapter={chapter} />
     </div>
   );
 }
